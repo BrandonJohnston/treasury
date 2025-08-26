53d233d6d9e18f0b1503710ec4c6aa6fce49d3da
Brandon Johnston <brandonjohnston@Brandons-MacBook-Pro.local>
2025-08-26 13:51:29 -0700

    add shacn components & login form

--- apps/web-frontend/src/app/login/page.tsx
+++ apps/web-frontend/src/app/login/page.tsx
@@ -1,7 +1,63 @@
+"use client";
+import { z } from 'zod';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+	email: z.email().max(50),
+	password: z.string().min(5, "Password must be more than 5 characters.").max(50, "Password must be less than 50 characters."),
+});
+
 export default function Login() {
+	// Define the form
+	const form = useForm<z.infer<typeof formSchema>>({
+		mode: "onBlur",
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			email: "",
+			password: "",
+		},
+	});
+
+	// Define submit handler
+	function onSubmit(values: z.infer<typeof formSchema>) {
+		console.log("onSubmit: ", values);
+	}
+
 	return (
-		<div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-			<p>Login page heyoooo</p>
+		<div className="login-form items-center justify-items-center w-full md:w-1/2 md:mx-auto p-10">
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col w-full max-w-100">
+					<FormField control={form.control}
+							   name="email"
+							   render={({ field }) => (
+								   <FormItem className="mb-6">
+									   <FormLabel>Email</FormLabel>
+									   <FormControl>
+										   <Input placeholder="you@example.com" {...field} />
+									   </FormControl>
+									   <FormMessage />
+								   </FormItem>
+							   )}
+				    />
+					<FormField control={form.control}
+							   name="password"
+							   render={({ field }) => (
+								   <FormItem className="mb-6">
+									   <FormLabel>Password</FormLabel>
+									   <FormControl>
+										   <Input type="password" placeholder="***" {...field} />
+									   </FormControl>
+									   <FormMessage />
+								   </FormItem>
+							   )}
+					/>
+					<Button type="submit" className="md:self-end cursor-pointer" disabled={!form.formState.isValid}>Submit</Button>
+				</form>
+			</Form>
 		</div>
 	);
 }
