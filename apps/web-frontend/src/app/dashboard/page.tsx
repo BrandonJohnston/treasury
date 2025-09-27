512391e07d657c1a1d2af65995cba81be5169203
Brandon Johnston <brandonjohnston@Brandons-MacBook-Pro.local>
2025-09-26 22:53:31 -0700

    login / logout and protected route

--- apps/web-frontend/src/app/dashboard/page.tsx
+++ apps/web-frontend/src/app/dashboard/page.tsx
@@ -1,6 +1,15 @@
-export default function Dashboard() {
+import { auth0 } from "@/lib/auth0";
+import { redirect } from "next/navigation";
+
+export default async function Dashboard() {
 	// Aliases
 	//
+
+	const session = await auth0.getSession();
+
+	if (!session) {
+		redirect("/");
+	}
 
 	return (
 		<div className="login-form items-center justify-items-center w-full md:w-1/2 md:mx-auto p-10">
--- apps/web-frontend/src/app/page.tsx
+++ apps/web-frontend/src/app/page.tsx
@@ -1,4 +1,13 @@
+import { auth0 } from "@/lib/auth0";
+import { redirect } from "next/navigation";
+
 export default async function Home() {
+
+	const session = await auth0.getSession();
+
+	if (session) {
+		redirect("/dashboard");
+	}
 
 	return (
 		<div className="flex font-sans">
