e24351ed5f7585c2473124c72fb59784db2a1b23
Brandon Johnston <brandonjohnston@Brandons-MacBook-Pro.local>
2025-09-27 14:49:30 -0700

    add nav toggle

--- apps/web-frontend/src/components/header/Header.tsx
+++ apps/web-frontend/src/components/header/Header.tsx
@@ -1,6 +1,7 @@
 import { auth0 } from "@/lib/auth0";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
 
 export default async function Header() {
 	const session = await auth0.getSession();
@@ -8,7 +9,12 @@ export default async function Header() {
 	console.log("session: ", session);
 
 	return (
-		<div className="absolute bg-sky-300 border-b-1 border-sky-800 flex items-center justify-between left-0 shadow-md top-0 w-full h-[60px]">
+		<div className="absolute bg-sky-300 border-b-1 border-sky-800 flex items-center justify-between left-0 shadow-md top-0 w-full h-[60px] px-[60px]">
+			<div className="absolute left-0 top-0 h-full w-[61px] flex items-center justify-center">
+				<Button variant="ghostNoHover" className="cursor-pointer">
+					<Menu className="size-6" />
+				</Button>
+			</div>
 			<h1 className="mx-[16px]">
 				<Link href="/">Treasury</Link>
 			</h1>
